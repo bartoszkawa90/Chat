dc5fd0d5c79e71a21a3e4681947544635b7be063
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-03-15 14:02:14 +0100

    something works

---
+++ main.py
@@ -0,0 +1,27 @@
+import time
+
+from Chat import *
+
+
+if __name__ == '__main__':
+    chat = Chat()
+
+    chat.open_chat_page()
+    chat.login_chat()
+
+    # TESTING ----------------------------------------------------------------------------------------------------------
+    test_input1 = 'what is the weather today'
+    test_input2 = 'what is the date today'
+    # ------------------------------------------------------------------------------------------------------------------
+    time.sleep(3)
+    chat.ask_chat(input=test_input1)
+    time.sleep(3)
+    chat.ask_chat(input=test_input2)
+    time.sleep(5)
+
+    answers = chat.get_answers(2)
+    print(answers)
+
+
+
+    print('a')
