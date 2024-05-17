757db0656e895603e9f162edd1ff0a25f81be7fe
Bartosz_Kawa <bartekkawa2@gmail.com>
2024-05-17 16:20:18 +0200

    Flask works fine

--- webapp.py
+++ webapp.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from templates import *
 
 app = Flask(__name__)
 
 
-@app.route('/')
+# example submit
+@app.route('/submit_form', methods=['GET', 'POST'])
+def submit():
+    if request.method == "POST":
+        value = ''
+        data = request.form['ask']
+        print("--- Received POST ---")
+        print(data)
+        sample = 'aaa'
+        return render_template("index.html", SAMPLE_DATA=sample)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+    sample = ''
+    if request.method == "POST":
+        print("--- Received POST ---")
+        data = request.form['ask']
+        print(data)
+        sample = data
+    return render_template("index.html", SAMPLE_DATA=sample)
 
 
 if __name__ == "__main__":
